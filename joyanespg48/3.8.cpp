d12662f9759aa4211cfdf144d2207cef1520c3b9
Edwin C. A <135646644+EdwinCastroA@users.noreply.github.com>
2023-06-09 23:40:58 -0500

    Update 3.8.cpp

--- joyanespg48/3.8.cpp
+++ joyanespg48/3.8.cpp
@@ -1,41 +1,50 @@
 #include <iostream>
-#include <cmath>
+using namespace std;
 
-void convertirAEfectivo(double cantidad) {  // DONDE SE LLAMA ESTA FUNCION
-    int billetes[] = {500, 200, 100, 50, 20, 10, 5, 2, 1};
-    int monedas[] = {50, 20, 10, 5, 2, 1};
+int main(int argc, char *argv[]){
+int quinientos, doscientos, cien, cincuenta, veinte, diez;
+int cinco, dos, uno, cincuentac, veintec, diezc, cincoc, dosc, unc;
+ 
+float CantidadOriginal;
+long int cantidad;
+cout << "Introduzca cantidad en euros ";
+cin >> CantidadOriginal;
     
-    std::cout << "Cambio óptimo en billetes y monedas:" << std::endl;
-
-    // Convertir a enteros para evitar errores de precisión en comparaciones
-    int cantidad_entera = static_cast<int>(round(cantidad * 100));
-
-    // Procesar los billetes
-    for (int i = 0; i < sizeof(billetes) / sizeof(billetes[0]); i++) { // QUE NOS INDICA ESTA LINEA DE CODIGO
-        int num_billetes = cantidad_entera / (billetes[i] * 100);
-        if (num_billetes > 0) {
-            std::cout << num_billetes << " billete(s) de " << billetes[i] << " euros" << std::endl;
-            cantidad_entera -= num_billetes * (billetes[i] * 100);
-        }
-    }
-
-    // Procesar las monedas
-    for (int i = 0; i < sizeof(monedas) / sizeof(monedas[0]); i++) {
-        int num_monedas = cantidad_entera / monedas[i];
-        if (num_monedas > 0) {
-            std::cout << num_monedas << " moneda(s) de " << monedas[i] << " céntimos" << std::endl;
-            cantidad_entera -= num_monedas * monedas[i];
-        }
-    }
-}
-
-int main() {
-    double cantidad;
-
-    std::cout << "Ingrese la cantidad en euros: ";
-    std::cin >> cantidad;
-
-    convertirAEfectivo(cantidad);
-
-    return 0;
+CantidadOriginal*=100; // se pasa de euros con decimales a centimos
+cantidad = (int) CantidadOriginal; // se trunca a centimos de euro
+quinientos = cantidad / 50000; cantidad = cantidad % 50000;
+doscientos = cantidad / 20000; cantidad = cantidad % 20000;
+cien = cantidad / 10000; cantidad = cantidad % 10000;
+cincuenta = cantidad / 5000; cantidad = cantidad % 5000;
+veinte = cantidad / 2000; cantidad = cantidad % 2000;
+diez = cantidad / 1000; cantidad = cantidad % 1000;
+cinco = cantidad / 500; cantidad = cantidad % 500;
+dos = cantidad / 200; cantidad = cantidad % 200;
+uno = cantidad / 100; cantidad =cantidad % 100;
+cincuentac = cantidad / 50; cantidad = cantidad % 50;
+veintec = cantidad / 20; cantidad = cantidad % 20;
+diezc = cantidad / 10; cantidad = cantidad % 10;
+cincoc = cantidad / 5; cantidad = cantidad % 5;
+dosc = cantidad / 2; cantidad = cantidad % 2;
+unc = cantidad;
+    
+cout << " cambio en moneda con el menor numero " << endl;
+cout << " cantidad original en centimos: "<< CantidadOriginal << endl;
+cout << " billetes de quinientos euros: " << quinientos << endl;
+cout << " billetes de doscientos euros: "<< doscientos << endl;
+cout << " billetes de cien euros : " << cien << endl;
+cout << " billetes de cincuenta euros: " << cincuenta << endl;
+cout << " billetes de veinte euros: "<< veinte << endl;
+cout << " billetes de diez euros : " << diez << endl;
+cout << " billetes de cinco euros: " << cinco << endl;
+cout << " monedad de dos euros: "<< dos << endl;
+cout << " monedad de un euro: " << uno << endl;
+cout << " monedas de cincuenta centimos de euros: "<<cincuentac<<endl;
+cout << " monedad de veinte centimos de eruro: "<< veintec << endl;
+cout << " monedad de diez centimos de euro: " << diezc << endl;
+cout << " monedas de cinco centimos de euros: " << cincoc << endl;
+cout << " monedad de dos centimos de eruro: "<< dosc << endl;
+cout << " monedad de un centimo de euro: " << unc << endl;
+system("PAUSE");
+return EXIT_SUCCESS;
 }
